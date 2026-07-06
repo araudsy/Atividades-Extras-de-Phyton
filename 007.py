861c91e5cacca3a8aab8c714f18c820b98b19cf8
Yasmin Araujo <yasminju07@gmail.com>
2026-07-06 15:26:34 -0300

    Create 007.py
    Peça o comprimento e a largura de um terreno retangular (em metros) e calcule a área em m². Depois converta essa área para hectares (1 hectare = 10.000 m²).

---
+++ 007.py
@@ -0,0 +1,5 @@
+comprimento = float(input('Digite o comprimento: '))
+largura = float(input('Digite a largura: '))
+area = largura * comprimento
+hec = area / 10000
+print('O terreno tem uma area de {:.2f}m². A a
